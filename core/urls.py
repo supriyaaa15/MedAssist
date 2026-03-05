c47e506506510421f7f746e8565d5b1d071e6bd4
Supriya Choudhary <supriyaaa155@gmail.com>
2026-03-05 19:58:44 +0530

    Landing page

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'),
+    path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
     path('patient-dashboard/', views.patient_dashboard, name='patient_dashboard'),
     path('doctor-dashboard/', views.doctor_dashboard, name='doctor_dashboard'),
     path('logout/', views.logout_view, name='logout'),
--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+def home(request):
+    return render(request, "home.html")
 
 def login_view(request):
     if request.method == "POST":
